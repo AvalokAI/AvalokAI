d817a9b6e5414b1cf64ab6d03d2e3444bdd8a411
Sankalp Garg <sankalp2621998@gmail.com>
2024-06-01 21:51:08 -0700

    model change

--- avalokai/embed/embed.py
+++ avalokai/embed/embed.py
@@ -6,7 +6,7 @@ from sentence_transformers import SentenceTransformer
 class Embed:
     def __init__(self, model_name="all-MiniLM-L6-v2") -> None:
         # model_name = "nlpaueb/legal-bert-base-uncased"
-        self.model = SentenceTransformer(model_name)
+        self.model = SentenceTransformer(model_name, trust_remote_code=True)
 
     def get_embedding_size(self):
         return self.model.get_sentence_embedding_dimension()
