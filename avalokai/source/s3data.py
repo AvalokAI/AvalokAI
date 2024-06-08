15bea4bd06cb7812e77d82cc7ce2e8981fadd33e
Sankalp Garg <sankalp2621998@gmail.com>
2024-06-08 00:40:07 -0700

    s3 data

--- avalokai/source/s3data.py
+++ avalokai/source/s3data.py
@@ -1,17 +1,31 @@
-from s3torchconnector import S3ClientConfig, S3IterableDataset, S3Reader
+import os
+
+from langchain_core.documents.base import Document
+from s3torchconnector import S3IterableDataset, S3Reader
 
 from ..embed.chunk import Chunk
 from .base_dataset import BaseDataset
 
 
-def s3_transform(sample: S3Reader):
-    bucket = sample.bucket
-    filename = sample.key
-    data = sample.read().decode("utf-8")
-    yield bucket, filename, data
-
-
 def get_s3_dataset(url: str, region: str, chunker: Chunk):
+
+    def s3_transform(sample: S3Reader):
+        bucket = sample.bucket
+        filename = sample.key
+        data = sample.read().decode("utf-8")
+        document = Document(page_content=data)
+        splits = chunker.get_chunks([document])
+
+        id = os.path.join("s3:/", bucket, filename)
+
+        for i, split in enumerate(splits):
+            item = {
+                "id": f"{id}-{i}",
+                "content": split.page_content,
+                "metadata": None,
+            }
+
+            yield item
 
     s3_iterable = S3IterableDataset.from_prefix(url, region=region)
     dataset = BaseDataset(s3_iterable, s3_transform)
